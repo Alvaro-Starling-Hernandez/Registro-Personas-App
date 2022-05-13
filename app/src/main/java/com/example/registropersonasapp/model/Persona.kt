25df5d7660d32eac89a301f24866e1a6a406c17b
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-05-12 22:34:38 -0400

    creando model persona

---
+++ app/src/main/java/com/example/registropersonasapp/model/Persona.kt
@@ -0,0 +1,18 @@
+package com.example.registropersonasapp.model
+
+import android.os.Parcelable
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "Personas")
+@Parcelize
+data class Persona(
+    @PrimaryKey(autoGenerate = true)
+    val personaId: Int,
+    val nombres: String,
+    val email: String,
+    val ocupacionId: Int,
+    val salario: Float
+
+): Parcelable
