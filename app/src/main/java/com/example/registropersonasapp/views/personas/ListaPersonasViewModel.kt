4501d892133855c41a0b6266fa440bbe0de5fd7a
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-05-12 22:40:27 -0400

    creando lista de personas view Model

---
+++ app/src/main/java/com/example/registropersonasapp/views/personas/ListaPersonasViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.registropersonasapp.views.personas
+
+import androidx.lifecycle.ViewModel
+
+class ListaPersonasViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
