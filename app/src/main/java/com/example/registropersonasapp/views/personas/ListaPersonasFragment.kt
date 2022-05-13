91a0b5a62dc12a40439c9a43969d5eb254716a25
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-05-12 22:32:16 -0400

    creando lista persononas fragment

---
+++ app/src/main/java/com/example/registropersonasapp/views/personas/ListaPersonasFragment.kt
@@ -0,0 +1,59 @@
+package com.example.registropersonasapp.views.personas
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.flowWithLifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
+import com.example.registropersonasapp.R
+import com.example.registropersonasapp.databinding.ActivityMainBinding
+import com.example.registropersonasapp.databinding.ListaPersonasFragmentBinding
+import com.example.registropersonasapp.model.Persona
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class ListaPersonasFragment : Fragment() {
+
+    private val viewModel: PersonasViewModel by viewModels()
+    private lateinit var binding: ListaPersonasFragmentBinding
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = ListaPersonasFragmentBinding.inflate(inflater, container ,false)
+
+        val adapter = PersonasAdapter(::openPersonasFragment)
+
+        binding.personasRecyclerView.adapter = adapter
+
+        lifecycleScope.launch {
+            viewModel.personas.flowWithLifecycle(lifecycle, Lifecycle.State.STARTED).
+            collect { lista ->
+                adapter.submitList(lista)
+            }
+        }
+
+        binding.personasButton.setOnClickListener {
+           openPersonasFragment()
+        }
+
+        return binding.root
+    }
+
+    fun openPersonasFragment(persona: Persona?=null)  {
+        val action = ListaPersonasFragmentDirections.actionToPersonasFragment(persona)
+        findNavController().navigate(action)
+    }
+
+}
