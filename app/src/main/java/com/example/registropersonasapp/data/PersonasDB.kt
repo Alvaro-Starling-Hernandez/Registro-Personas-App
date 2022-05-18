74db970d6b9ffde8023fc2b07aece365d6c0275b
Alvaro S. Hernandez Luzon <alvarostarling24@gmail.com>
2022-05-17 23:39:38 -0400

    Agregando tabla ocupaciones

--- app/src/main/java/com/example/registropersonasapp/data/PersonasDB.kt
+++ app/src/main/java/com/example/registropersonasapp/data/PersonasDB.kt
@@ -2,12 +2,14 @@ package com.example.registropersonasapp.data
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.example.registropersonasapp.model.Ocupacion
 import com.example.registropersonasapp.model.Persona
 
 @Database(
-    entities = [Persona::class],
-    version = 1
+    entities = [Persona::class,Ocupacion::class],
+    version = 2
 )
 abstract class PersonasDB : RoomDatabase(){
     abstract val personaDao: PersonaDao
+    abstract val ocupacionesDao: OcupacionesDao
 }
